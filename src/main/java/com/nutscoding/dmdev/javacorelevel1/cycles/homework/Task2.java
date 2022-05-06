3e51022ce63bd2927eb2a3908aedb91825540b74
Fedor <shlakmail@list.ru>
2022-05-06 15:58:01 +0300

    init commit

--- src/main/java/com/nutscoding/dmdev/javacorelevel1/cycles/homework/Task2.java
+++ src/main/java/com/nutscoding/dmdev/javacorelevel1/cycles/homework/Task2.java
@@ -10,23 +10,22 @@ package com.nutscoding.dmdev.javacorelevel1.cycles.homework;
  */
 public class Task2 {
     public static void main(String[] args) {
+        int value = 26748593;
+        System.out.println(reverse(value));
+    }
 
+    public static int reverse(int value) {
+        int result = 0;
+        String a = String.valueOf(0);
+        while (value % 10 > 1) {
+            a = a + value % 10;
+            value = value / 10;
+            result = Integer.parseInt(a);
+        }
+        return result;
     }
 }
 
 
 
-//        int value = 26748593;
-//        System.out.println(reverse(value));
-//    }
-//
-//    public static int reverse(int value) {
-//        int result = 0;
-//        String a = String.valueOf(0);
-//        while (value % 10 > 1){
-//            a = a + value % 10;
-//            value = value / 10;
-//            result = Integer.parseInt(a);
-//        }
-//        return result;
-//    }
+
